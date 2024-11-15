3c8a1336464d5a637da8ca528eaa9c81d613be86
Samuel Varga <sam@vargasd.com>
2024-11-15 16:19:21 -0500

    Update init script

--- .github/README.md
+++ .github/README.md
@@ -1,7 +1,8 @@
 # Steps for setup
 
 1. [Install Homebrew](https://docs.brew.sh/Installation)
-1. Run setup script: `./init.sh`
+1. Run setup script: `./scripts/init.sh`
+1. Run stow: `stow .`
 1. Install [JetBrains Mono](https://github.com/JetBrains/JetBrainsMono)
 
 ## Updating Installed Packages
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-set -e
-
 dir=$(dirname "${BASH_SOURCE[0]}")
 
-stow -d $dir/.. -t $HOME
-
-brew bundle install
+brew bundle --file $dir/../exclude/Brewfile
 
 $dir/mac-settings.sh
 
+git submodule init
+git submodule update
+
 # registers additional bat themes/languages
 bat cache --build
+
+# install/start skhd (may need a computer restart)
+skhd --install-service
+skhd --start-service
+
+# install asdf tools
+cat .tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -I {} asdf plugin add  {}
+asdf install
