9c39cce262c19ab84a02a84b7427b5031bb68297
Samuel Varga <sam@vargasd.com>
2024-12-30 07:57:18 -0500

    Add rounded borders for signature help

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -610,6 +610,9 @@ require("lazy").setup({
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 				border = "rounded",
 			})
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = "rounded",
+			})
 
 			require("lsp-format").setup({})
 
