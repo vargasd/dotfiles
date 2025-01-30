b20204fff7a525528c06b15076fdfaab5a0edd94
Samuel Varga <sam@vargasd.com>
2025-01-30 07:10:41 -0500

    Switch to mini surround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1219,13 +1219,23 @@ require("lazy").setup({
 	},
 
 	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
+		"echasnovski/mini.surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			mappings = {
+				add = "<C-s>a", -- Add surrounding in Normal and Visual modes
+				delete = "<C-s>d", -- Delete surrounding
+				find = "<C-s>f", -- Find surrounding (to the right)
+				find_left = "<C-s>F", -- Find surrounding (to the left)
+				highlight = "<C-s>h", -- Highlight surrounding
+				replace = "<C-s>r", -- Replace surrounding
+				update_n_lines = "", -- Update `n_lines`
+
+				suffix_last = "l", -- Suffix to search with "prev" method
+				suffix_next = "n", -- Suffix to search with "next" method
+			},
+		},
 	},
 
 	{
