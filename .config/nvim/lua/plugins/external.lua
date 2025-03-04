d3bf3935a63e7199f13b6d3063a27aab29aa6263
Samuel Varga <sam@tildei.com>
2025-03-04 06:44:10 -0500

    Remove silly boilerplate comment

--- .config/nvim/lua/plugins/external.lua
+++ .config/nvim/lua/plugins/external.lua
@@ -13,7 +13,6 @@ return {
 			"DBUIFindBuffer",
 		},
 		init = function()
-			-- Your DBUI configuration
 			vim.g.db_ui_use_nerd_fonts = 1
 			vim.g.db_ui_auto_execute_table_helpers = 1
 			vim.g.db_ui_winwidth = 25
